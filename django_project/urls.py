8455f3d92289e3965c0685e041bee53fc577ab1a
JustinLi3 <lijustin83@gmail.com>
2024-12-30 13:26:31 -0500

    Finished password with email reset and added ui on login page

--- django_project/urls.py
+++ django_project/urls.py
@@ -31,8 +31,10 @@ urlpatterns = [
     #Password reset view (route that provides a user with a form to fill out that would send a password reset instructions to the email)
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    #Confirmation for added layer of security, with tokens verifying user 
+    #Confirmation for added layer of security, with tokens verifying user (essentially the view of the link opened from the email)
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
     path('profile/', user_views.profile, name = 'profile' )
 ] 
 
