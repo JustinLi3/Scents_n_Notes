55cf0fe8b100a27a2d8cbabd296b5a185da8040a
JustinLi3 <lijustin83@gmail.com>
2025-01-18 23:29:01 -0500

    Fixed/updated form, creating prompt within JS

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -6,27 +6,60 @@ $(document).ready(function() {
 
   // parsing user preferences 
   $("#preferences").on("submit", function (event){   
-    const fragranceFormat = $("#fragrance-format");
-    const scentPortfolio = $("#scent-portfolio");
-    const errorMessage = $("#error-message");
+    let email = $("#preference-email").val();
+    let fragranceType = "Fragrance Type: " + $("#fragrance-type").val() + " ";
+    let fragranceFormat = "";
+    let scentPortfolio = "";  
+    let additionalComments = 
 
+    // Error message to return to user 
+    const errorMessage = $("#error-message"); 
+    // Manual validation
     let valid = true;  
-    let message = "";
+    // Message back to error
+    let message = ""; 
 
-    if (fragranceFormat.length === 1){ 
+
+    //For each option selected for each multiselector, append the value
+    $("#fragrance-format").find('option:selected').each(function(index, element){ 
+      fragranceFormat += (element.value+", ");
+    }); 
+
+    $("#scent-portfolio").find('option:selected').each(function(index, element){ 
+      scentPortfolio += (element.value+", ");
+    }); 
+
+    //Check whether either were selected, otherwise start prompt
+    if (!fragranceFormat){
       message += "Please select at least one fragrance format before submitting.<br>"; 
       valid = false;
     }
-    if (scentPortfolio.length === 1){
-      message += "Please select at least one scent portfolio before submitting.<br>";
-      valid = false;
+    else{  
+      fragranceFormat = fragranceFormat.slice(0,-2);
+      fragranceFormat = "Fragrance Format(s): " + fragranceFormat;
     } 
+
+    if (!scentPortfolio){
+      message += "Please select at least one scent portfolio before submitting.<br>"; 
+      valid = false;
+    }
+    else{
+      scentPortfolio = scentPortfolio.slice(0,-2);
+      scentPortfolio = "Fragrance Portfolio(s): " + scentPortfolio;
+    } 
+    alert(email);
+    alert(fragranceType);
+    alert(fragranceFormat);
+    alert(scentPortfolio);
+
     if (!valid){ 
       event.preventDefault(); 
       errorMessage.html(message); 
       errorMessage.css("display", "block");
     }
-    else{
+    else{ 
+      event.preventDefault(); 
+
       errorMessage.css("display", "none");
     }
   });
