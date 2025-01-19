a84c39a8e6dce4b8dda655bf0319c1e5a8501d29
JustinLi3 <lijustin83@gmail.com>
2025-01-18 20:17:34 -0500

    Saving

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
-
   // multiselect customization
-    $(".multiple-checkboxes").multiselect({
-      includeSelectAllOption: true,
-    });
+  $(".multiple-checkboxes").multiselect({
+    includeSelectAllOption: true,
+  });
 
   // parsing user preferences 
-  $("#preferences").on("submit", function (event){ 
-    alert("Handler Called!");
-    event.preventDefault();
-  })
+  $("#preferences").on("submit", function (event){   
+    const fragranceFormat = $("#fragrance-format");
+    const scentPortfolio = $("#scent-portfolio");
+    const errorMessage = $("#error-message");
+
+    let valid = true;  
+    let message = "";
+
+    if (fragranceFormat.length === 1){ 
+      message += "Please select at least one fragrance format before submitting.<br>"; 
+      valid = false;
+    }
+    if (scentPortfolio.length === 1){
+      message += "Please select at least one scent portfolio before submitting.<br>";
+      valid = false;
+    } 
+    if (!valid){ 
+      event.preventDefault(); 
+      errorMessage.html(message); 
+      errorMessage.css("display", "block");
+    }
+    else{
+      errorMessage.css("display", "none");
+    }
+  });
 });
